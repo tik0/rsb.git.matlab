1ef24565a67f914f72bc1fa506d1af0b6d168844
Sebastian Wrede <swrede@techfak.uni-bielefeld.de>
2011-10-17 08:34:21 +0000

    add some documentation

--- src/rsb/matlab/JointAnglesQueue.java
+++ src/rsb/matlab/JointAnglesQueue.java
@@ -3,18 +3,43 @@
  */
 package rsb.matlab;
 
+import java.util.concurrent.TimeUnit;
+
 import rsb.util.QueueAdapter;
 import rst.kinematics.JointAnglesType.JointAngles;
 
 /**
+ * Simple queuing callback adapting generic push-style RSBJava
+ * callback interface to a simple, specialized and Matlab-
+ * compatible pull-style interface.
+ * 
  * @author swrede
  *
  */
 public class JointAnglesQueue extends QueueAdapter<JointAngles> {
 	
-	// TODO extract take into Matlab Queue signature...
+	/**
+	 * Delegate function to return queue objects as a list.
+	 * It blocks until a result is available. Returned objects
+	 * are removed from the underlying blocking queue.
+	 * 
+	 * @return List of angles, see rst.kinematics.JointAngles
+	 * @throws InterruptedException
+	 */
 	public Object[] take() throws InterruptedException {
 		return getQueue().take().getAnglesList().toArray();
-	} 	
+	} 
+
+	/**
+	 * Delegate function to return queue objects as a list.
+	 * It blocks until a result is available. Returned objects
+	 * are removed from the underlying blocking queue.
+	 * 
+	 * @return List of angles, see rst.kinematics.JointAngles
+	 * @throws InterruptedException
+	 */	
+	public Object[] take(int ms) throws InterruptedException {
+		return getQueue().poll(ms, TimeUnit.MILLISECONDS).getAnglesList().toArray();
+	}
 	
 }
