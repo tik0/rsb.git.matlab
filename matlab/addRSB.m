d68e8852ae6b934b68fe91a3b81f596b27fc680d
Sebastian Wrede <swrede@techfak.uni-bielefeld.de>
2011-10-20 11:45:11 +0000

    added sample rst.kinematics.JointAngles data for testing

--- matlab/addRSB.m
+++ matlab/addRSB.m
@@ -1,10 +1,11 @@
 function [ factory ] = addRSB()
 %ADDRSX Add RSX libraries to Matlab
 %   Detailed explanation goes here
-javaaddpath /usr/share/java/RSBJava-0.5.0.jar
-javaaddpath /usr/share/java/protobuf.jar
-javaaddpath /usr/share/java/rstsandbox.jar
-javaaddpath /usr/share/java/rst.jar
-javaaddpath /public/amarsi_workshop/lib/rsb.m-0.5.0.jar
+rootpath = '/vol/cit/share/java';
+javaaddpath ([rootpath '/RSBJava-0.5.0.jar'])
+javaaddpath /opt/local/share/java/protobuf.jar
+javaaddpath ([rootpath '/rstsandbox.jar'])
+javaaddpath ([rootpath '/rst.jar'])
+javaaddpath /Users/swrede/Workspace/RSB.m/dist/lib/rsb.m-0.5.0.jar
 rsb.matlab.ConverterRegistration.register()
 end
--- matlab/plotAllAngles.m
+++ matlab/plotAllAngles.m
@@ -1,15 +1,15 @@
-function [ ] = plotAngles( queue )
+function [ ] = plotAllAngles( queue )
 %PLOT1D Summary of this function goes here
 %   Detailed explanation goes here
 a = zeros(8,200)
 while(1)
 a(:,1:end - 1) = a(:,2:end);
 % blocks until new data is available
-b = queue.take(10000)
+b = angles.take(10000)
 c = parseArray(b);
 a(:,end) = c;
-queue.getQueue.clear;
-plot(a);
+angles.getQueue.clear;
+plot(a');
 getframe;
 end
 end
