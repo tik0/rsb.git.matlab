6523ea3fe8bb7304d8cb573ab6dd3037c278c925
Sebastian Wrede <swrede@techfak.uni-bielefeld.de>
2011-10-30 18:43:16 +0000

    started thinking about image queue, refs #681

---
+++ src/rsb/matlab/ImageQueue.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package rsb.matlab;
+
+import java.util.concurrent.TimeUnit;
+
+import rsb.util.QueueAdapter;
+import rst.vision.ImageType.Image;
+
+/**
+ * Simple queuing callback adapting generic push-style RSBJava
+ * callback interface to a simple, specialized and Matlab-
+ * compatible pull-style interface.
+ * 
+ * @author swrede
+ *
+ */
+public class ImageQueue extends QueueAdapter<Image> {
+
+
+	/**
+	 * Delegate function to return queue objects as a list.
+	 * It blocks until a result is available. Returned objects
+	 * are removed from the underlying blocking queue.
+	 * 
+	 * @return raw image data, see rst.vision.Image
+	 * @throws InterruptedException
+	 */	
+	public byte[] take(int ms) throws InterruptedException {
+		Image result = getQueue().poll(ms, TimeUnit.MILLISECONDS);
+		if (result==null) return null;
+		// else just return the raw data 
+		return result.getData().toByteArray();
+	}
+	
+}
