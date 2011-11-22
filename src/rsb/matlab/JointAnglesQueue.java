4c959ca004e1d6a958aae479cb969ce046ab98f8
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2011-11-22 08:32:49 +0000

    Consistent coding style

--- src/rsb/matlab/JointAnglesQueue.java
+++ src/rsb/matlab/JointAnglesQueue.java
@@ -6,39 +6,41 @@ import rsb.util.QueueAdapter;
 import rst.kinematics.JointAnglesType.JointAngles;
 
 /**
- * Simple queuing callback adapting generic push-style RSBJava
- * callback interface to a simple, specialized and Matlab-
- * compatible pull-style interface.
+ * Simple queuing callback adapting generic push-style RSBJava callback
+ * interface to a simple, specialized and Matlab- compatible pull-style
+ * interface.
  * 
  * @author swrede
  */
 public class JointAnglesQueue extends QueueAdapter<JointAngles> {
-	
+
 	/**
-	 * Delegate function to return queue objects as a list.
-	 * It blocks until a result is available. Returned objects
-	 * are removed from the underlying blocking queue.
+	 * Delegate function to return queue objects as a list. It blocks until a
+	 * result is available. Returned objects are removed from the underlying
+	 * blocking queue.
 	 * 
 	 * @return List of angles, see rst.kinematics.JointAngles
 	 * @throws InterruptedException
 	 */
 	private Object[] take() throws InterruptedException {
 		return getQueue().take().getAnglesList().toArray();
-	} 
+	}
 
 	/**
-	 * Delegate function to return queue objects as a list.
-	 * It blocks until a result is available. Returned objects
-	 * are removed from the underlying blocking queue.
+	 * Delegate function to return queue objects as a list. It blocks until a
+	 * result is available. Returned objects are removed from the underlying
+	 * blocking queue.
 	 * 
 	 * @return List of angles, see rst.kinematics.JointAngles
 	 * @throws InterruptedException
-	 */	
+	 */
 	public Object[] take(int ms) throws InterruptedException {
 		JointAngles result = getQueue().poll(ms, TimeUnit.MILLISECONDS);
-		if (result==null) return null;
+		if (result == null) {
+			return null;
+		}
 		// else just return the object list
 		return result.getAnglesList().toArray();
 	}
-	
+
 }
--- src/rsb/matlab/MatrixDoubleQueue.java
+++ src/rsb/matlab/MatrixDoubleQueue.java
@@ -7,36 +7,39 @@ import rsb.util.QueueAdapter;
 import rst.math.MatrixDoubleType.MatrixDouble;
 
 /**
- * Simple queuing callback adapting generic push-style RSBJava
- * callback interface to a simple, specialized and Matlab-
- * compatible pull-style interface.
+ * Simple queuing callback adapting generic push-style RSBJava callback
+ * interface to a simple, specialized and Matlab- compatible pull-style
+ * interface.
  * 
  * @author cemmeric
  */
 public class MatrixDoubleQueue extends QueueAdapter<MatrixDouble> {
 
 	/**
-	 * Delegate function to return queue objects as a list.
-	 * It blocks until a result is available. Returned objects
-	 * are removed from the underlying blocking queue.
+	 * Delegate function to return queue objects as a list. It blocks until a
+	 * result is available. Returned objects are removed from the underlying
+	 * blocking queue.
 	 * 
 	 * @return 2-dim double array of matrix entries, see rst.math.MatrixDouble
 	 * @throws Exception
-	 */	
-	public double[][] take(int ms) throws Exception  {
+	 */
+	public double[][] take(int ms) throws Exception {
 		MatrixDouble matrix = getQueue().poll(ms, TimeUnit.MILLISECONDS);
-	
-		if (matrix==null) return null;
+
+		if (matrix == null) {
+			return null;
+		}
 
 		// check dimensions
 		int M = matrix.getSize().getM();
 		int N = matrix.getSize().getN();
-		if (matrix.getData().getValueCount() != M*N) {
-			throw new Exception("Dimension missmatch between expected M*N values and provided number of values");
+		if (matrix.getData().getValueCount() != M * N) {
+			throw new Exception(
+					"Dimension missmatch between expected M*N values and provided number of values");
 		}
 
 		Iterator<Double> it = matrix.getData().getValueList().iterator();
-		// create double array 
+		// create double array
 		double[][] retval = new double[M][N];
 		for (int m = 0; m < M; m++) {
 			for (int n = 0; n < N; n++) {
@@ -46,5 +49,5 @@ public class MatrixDoubleQueue extends QueueAdapter<MatrixDouble> {
 
 		return retval;
 	}
-	
+
 }
