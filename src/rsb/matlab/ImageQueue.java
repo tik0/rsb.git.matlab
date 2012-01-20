160fe3c15f4a45bfe69e6304467af18c1838744f
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2012-01-20 10:35:49 +0000

    fix indentation

--- src/rsb/matlab/ImageQueue.java
+++ src/rsb/matlab/ImageQueue.java
@@ -23,9 +23,9 @@ public class ImageQueue extends QueueAdapter<Image> {
 	 * @return raw image data, see rst.vision.Image
 	 * @throws InterruptedException
 	 */
-        public Image takeImage(int ms) throws InterruptedException {
-                return getQueue().poll(ms, TimeUnit.MILLISECONDS);
-        }
+	public Image takeImage(int ms) throws InterruptedException {
+		return getQueue().poll(ms, TimeUnit.MILLISECONDS);
+	}
 
 	public byte[] take(int ms) throws InterruptedException {
 		Image result = getQueue().poll(ms, TimeUnit.MILLISECONDS);
@@ -39,5 +39,5 @@ public class ImageQueue extends QueueAdapter<Image> {
 	public ImageQueue(final int capacity, final boolean discardOldest) {
 		super(capacity, discardOldest);
 	}
-	
+
 }
