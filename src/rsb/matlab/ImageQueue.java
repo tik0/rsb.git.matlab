06b3bb7ce072010981b44fb679a30705a1f15f91
Šimon Fojtů <fojtusim@fel.cvut.cz>
2012-01-20 10:20:34 +0000

    Adding a method to take Image from a queue (fixes issue #809).

--- src/rsb/matlab/ImageQueue.java
+++ src/rsb/matlab/ImageQueue.java
@@ -23,6 +23,10 @@ public class ImageQueue extends QueueAdapter<Image> {
 	 * @return raw image data, see rst.vision.Image
 	 * @throws InterruptedException
 	 */
+        public Image takeImage(int ms) throws InterruptedException {
+                return getQueue().poll(ms, TimeUnit.MILLISECONDS);
+        }
+
 	public byte[] take(int ms) throws InterruptedException {
 		Image result = getQueue().poll(ms, TimeUnit.MILLISECONDS);
 		if (result == null) {
